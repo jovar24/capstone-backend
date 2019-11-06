be6e2b8243235277d35b3668110db70f3e4f7b39
Jose Vargas <jovar2407@gmail.com>
2019-11-06 14:55:36 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), unique= False)
+    content = db.Column(db.String(300), unique=False)
+    
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+class ReviewSchema(ma.Schema):
+    class Meta:
+        fields = ['id','title', 'content']
+
+review_schema = ReviewSchema()
+reviews_schemas = ReviewSchema(many = True)     
+
+@app.route('/review', methods=["POST"])
+def post_review():
+    title = request.json['title']
+    content = request.json['content']
+
+    new_review = Review(title, content)
+
+    db.session.add(new_review)
+    db.session.commit()
+
+    review = Review.query.get(new_review.id)
+
+    return review_schema.jsonify(review)
+
+@app.route('/reviews', methods= ["GET"])
+def get_reviews():
+        all_reviews = Review.query.all()
+        result = reviews_schemas.dump(all_reviews)
+        return jsonify(result) 
+
+@app.route('/review/<id>', methods=["DELETE"])
+def erease_rev(id):
+    review = Review.query.get(id)
+    db.session.delete(review)
+    db.session.commit()        
+
+    return "Review has been deleted"
+
+if __name__ == '__main__':
+    app.run(debug=True)
